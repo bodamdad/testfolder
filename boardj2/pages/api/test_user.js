6feb940ad07248f3525f62b76677ecb307b3f53b
bodamdad <ljlj123@nate.com>
2023-10-18 16:35:40 +0900

    db 테스트

--- boardj2/pages/api/test_user.js
+++ boardj2/pages/api/test_user.js
@@ -2,8 +2,8 @@ import pool from '/db/database.js';
 
 export default async (req, res) => {
     try {
-      const [members] = await pool.query('SELECT * FROM t1;');
-      res.json(members);
+      const [t1] = await pool.query('SELECT * FROM t1;');
+      res.json(t1);
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
--- boardj2/src/app/page.js
+++ boardj2/src/app/page.js
@@ -1,18 +1,16 @@
 'use client'
-import Image from 'next/image'
-import styles from './page.module.css'
 import { useState, useEffect } from 'react';
-
+import Link from 'next/link';
 
 export default function Home() {
 
-  const [test_user, setMembers] = useState([]);
+  const [test_user, setTest_user] = useState([]);
 
   useEffect(() => {
-    const fetchMembers = async () => {
-      const response = await fetch('/api/test_user');
+      const fetchMembers = async () => {
+      const response = await fetch('/api/dbj');
       const data = await response.json();
-      setMembers(data);
+      setTest_user(data);
     };
 
     fetchMembers();
@@ -21,14 +19,28 @@ export default function Home() {
   return (
     <div>
       <h1>Members</h1>
-      <span>{test_user.username}</span>
+      <Link href="/guide">GUIDE</Link>
       <ul>
-        {test_user.map(member => (
-          <li key={member.user_id}>
-            No: {member.user_id}, ID: {member.user_name}
+        {test_user.map(({wr_num, wr_subject, wr_content}) => (
+          <li key={wr_num}>
+            <p>{wr_subject}</p>
+            <p>{wr_content}</p>
           </li>
         ))}
       </ul>
+
+          {
+            <ul>
+              <li>
+                <img src="" />
+                <p></p>
+              </li>
+            </ul>
+          }
+
+
+
+      {/* <span>{test_user[1].user_name}</span> */}
     </div>
   );
 }
